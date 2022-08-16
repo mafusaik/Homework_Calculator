e801503a227bbe9de911c05f810e87720334e017
Arseni Hlaz <mafusaik@gmail.com>
2022-08-16 20:33:13 +0300

    working version 2.0 fix scalar regex

---
+++ app/src/main/java/by/homework/hlazArseni/calculator/button_functions/allClearAction.kt
@@ -0,0 +1,8 @@
+package by.homework.hlazArseni.calculator.button_functions
+
+import by.homework.hlazArseni.calculator.databinding.FragmentBinding
+
+fun allClearAction(binding: FragmentBinding) {
+        binding.mathOperation.text = ""
+        binding.resultText.text = ""
+    }
